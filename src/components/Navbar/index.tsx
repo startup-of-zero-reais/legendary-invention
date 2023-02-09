90bace31e5c50c601cc4d5ae29e4112019801a74
vicente_nascimento <vicente19981@live.com>
2023-02-09 18:03:19 -0300

    refactor: change navigation paths

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,20 +1,16 @@
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import Image from "next/image";
-import clsx from "clsx";
 import { Fragment } from "react";
 import { CONSTANTS } from "@/constants";
+import classNames from "@/utils/add-classname";
 
 const navigation = [
-  { name: "Encontrar Projeto", href: "#", current: true },
-  { name: "Meus Projetos", href: "#", current: false },
+  { name: "Encontrar Vagas", href: "#", current: true },
+  { name: "Minhas Vagas", href: "#", current: false },
 ];
 
 const profile_settings = ["Perfil", "Configurações", "Sair"];
-
-function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(" ");
-}
 
 export default function Navbar() {
   return (
@@ -42,8 +38,8 @@ export default function Navbar() {
                     <Image
                       src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
                       alt="Workflow"
-                      width={50}
-                      height={50}
+                      width={35}
+                      height={35}
                     />
                     <h1 className="ml-3 font-medium">
                       {CONSTANTS.name_application}
@@ -53,8 +49,8 @@ export default function Navbar() {
                     <Image
                       src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
                       alt="Workflow"
-                      width={50}
-                      height={50}
+                      width={35}
+                      height={35}
                     />
                     <h1 className="ml-3 font-medium">
                       {CONSTANTS.name_application}
@@ -62,13 +58,13 @@ export default function Navbar() {
                   </div>
                 </div>
                 <div className="hidden sm:ml-6 sm:block">
-                  <div className="flex space-x-4 ml-10">
+                  <div className="flex space-x-4 ml-20">
                     {navigation.map((item) => (
                       <a
                         className={classNames(
                           item.current
                             ? `text-blue-400 font-normal`
-                            : "text-gray-300 font-light hover:bg-gray-700 hover:text-white",
+                            : "text-gray-400 font-light hover:bg-gray-700 hover:text-white",
                           "rounded-md text-sm relative px-2 py-2"
                         )}
                         key={item.name}
@@ -91,8 +87,8 @@ export default function Navbar() {
                     className="h-8 w-8 rounded-full"
                     src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                     alt="Perfil de usuário"
-                    width={50}
-                    height={50}
+                    width={35}
+                    height={35}
                   />
                 </Menu.Button>
                 <Transition
