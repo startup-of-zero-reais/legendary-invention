bcda291c34cf7e933423e09ca3c7f92d456d0746
vicente_nascimento <vicente19981@live.com>
2023-02-14 00:18:11 -0300

    feat: create constants applications

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,4 +2,12 @@ export const CONSTANTS = {
   get name_application() {
     return "JOBITE";
   },
+
+  get description_application() {
+    return `${this.name_application} oference uma grande variedade de vagas voltado para tecnologia. Também oferecemos suporte para empresas que estão procurando profissionais.`;
+  },
+
+  get price_filter() {
+    return [2000, 50000];
+  },
 };
