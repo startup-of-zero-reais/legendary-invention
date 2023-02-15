0fbd3b4e4ab45f94a8901718f153cd4a2ac9d946
vicente_nascimento <vicente19981@live.com>
2023-02-15 01:07:10 -0300

    feat: adding a container and flex position

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
 import Head from "next/head";
-import { Header, Filter } from "@/components";
-import { Container } from "@chakra-ui/react";
+import { Header, Filter, CardVacancies } from "@/components";
+import {
+  Box,
+  Container,
+  Flex,
+  IconButton,
+  Input,
+  useColorModeValue,
+  VStack,
+} from "@chakra-ui/react";
 import { CONSTANTS } from "@/constants";
+import { Search2Icon } from "@chakra-ui/icons";
 
 export default function Home() {
   return (
@@ -13,8 +22,49 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <Container maxW="7xl" pt="5">
-        <Filter />
+
+      <Container maxW="container.lg" minHeight="calc(100vh - 68px)">
+        <Flex
+          minH="100%"
+          p={{ base: "2", md: "4", lg: "6" }}
+          gap={{ base: 2, md: 4, lg: 6 }}
+          flexDirection={{ base: "column", lg: "row" }}
+        >
+          <Box>
+            <Filter />
+          </Box>
+          <Flex flexDirection="column" gap={{ base: 2, md: 4, lg: 6 }} w="full">
+            <Box>
+              <Flex
+                bg="white"
+                p={{ base: "2", md: "4", lg: "6" }}
+                border="1px"
+                borderColor={useColorModeValue("gray.200", "gray.700")}
+                borderRadius={{ base: "md", lg: "2xl" }}
+              >
+                <Input placeholder="Procurar vagas por título, empresa ou qualquer palavra chave..." />
+                <IconButton
+                  aria-label="Procurador de vagas"
+                  icon={<Search2Icon />}
+                  ml="2"
+                />
+              </Flex>
+            </Box>
+            <VStack
+              p={{ base: "2", md: "4", lg: "6" }}
+              gap={{ base: 0, md: 2, lg: 4 }}
+              bg="white"
+              border="1px"
+              borderColor={useColorModeValue("gray.200", "gray.700")}
+              borderRadius={{ base: "md", lg: "2xl" }}
+              alignItems={"stretch"}
+            >
+              {[0, 1, 2, 3].map((_, key) => (
+                <CardVacancies index={key} key={key} />
+              ))}
+            </VStack>
+          </Flex>
+        </Flex>
       </Container>
     </>
   );
