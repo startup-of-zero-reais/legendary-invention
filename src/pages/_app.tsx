87d18fd02fc580d50781269ddd03139616d6f638
vicente_nascimento <vicente19981@live.com>
2023-02-15 01:04:47 -0300

    feat: adding default font family

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,17 @@ import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 
 const theme = extendTheme({
+  components: {
+    Text: {
+      baseStyle: {
+        fontFamily: "Poppins",
+      },
+    },
+  },
   colors: {
     gray: {
       50: "#F7F5F8",
+      300: "#444242",
     },
   },
 });
