3c7f5e7db073ccc0139cebf37b7e15ba21c3ccd5
Vicente Junior <vicente19981@live.com>
2023-02-15 23:55:38 -0300

    feat: adding props on Card vacancies

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -60,7 +60,14 @@ export default function Home() {
               alignItems={"stretch"}
             >
               {[0, 1, 2, 3].map((_, key) => (
-                <CardVacancies index={key} key={key} />
+                <CardVacancies index={key} key={key}
+                  createdAt={new Date()}
+                  description={'Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.'}
+                  salary={{ min: 1200, max: 2000 }}
+                  tags={['Frontend', 'Backend']}
+                  title={'Procurando desenvolvedor FullStack'}
+                  workModel={'Home Office'}
+                />
               ))}
             </VStack>
           </Flex>
