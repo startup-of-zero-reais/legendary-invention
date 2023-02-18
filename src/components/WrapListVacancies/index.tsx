86283f64ef53d997a3cbd5cc4b8a0bba21b89e37
vicente_nascimento <vicente19981@live.com>
2023-02-18 15:05:18 -0300

    feat: adding WrapListVacancies

---
+++ src/components/WrapListVacancies/index.tsx
@@ -0,0 +1,29 @@
+import { JobModel } from "@/domain";
+import { useColorModeValue, VStack } from "@chakra-ui/react";
+import React from "react";
+import CardVacancy from "../CardVacancy";
+
+type Props = {
+  isLoading: boolean;
+  jobs?: JobModel[];
+};
+
+const WrapListVacancies: React.FC<Props> = ({ isLoading, jobs }) => {
+  return (
+    <VStack
+      p={{ base: "2", md: "4", lg: "6" }}
+      gap={{ base: 0, md: 2, lg: 4 }}
+      bg="white"
+      border="1px"
+      borderColor={useColorModeValue("gray.200", "gray.700")}
+      borderRadius={{ base: "md", lg: "2xl" }}
+      alignItems={"stretch"}
+    >
+      {jobs?.map((job, key) => (
+        <CardVacancy index={key} key={key} job={job} isLoading={isLoading} />
+      ))}
+    </VStack>
+  );
+};
+
+export default WrapListVacancies;
