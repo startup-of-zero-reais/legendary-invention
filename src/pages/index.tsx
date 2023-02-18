a686623eb192d6f89e5e1a0a6a41823325c0b21f
vicente_nascimento <vicente19981@live.com>
2023-02-18 15:05:32 -0300

    feat: using component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,23 @@
 import Head from "next/head";
-import { Header, Filter, CardVacancy, ModalApplyVacancy } from "@/components";
-import {
-  Box,
-  Container,
-  Flex,
-  IconButton,
-  Input,
-  useColorModeValue,
-  useDisclosure,
-  VStack,
-} from "@chakra-ui/react";
+import { Header, Filter, ModalApplyVacancy, Search } from "@/components";
+import { Container, Flex, useDisclosure } from "@chakra-ui/react";
 import { CONSTANTS } from "@/constants";
-import { Search2Icon } from "@chakra-ui/icons";
 import { useRouter } from "next/router";
-import Link from "next/link";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useLoadJob, useLoadAllJob } from "@/domain";
+import WrapListVacancies from "@/components/WrapListVacancies";
 
 export default function Home() {
   let router = useRouter();
   const { onOpen, isOpen, onClose } = useDisclosure();
+  const [search, setSearch] = useState("");
 
-  const { data: listJob, isLoading: isLoadingListJob } = useLoadAllJob({
-    search: "Professor",
+  const {
+    data: listJob,
+    isLoading: isLoadingListJob,
+    refetch: refetchListJob,
+  } = useLoadAllJob({
+    search,
   });
 
   const { data: job } = useLoadJob({
@@ -33,6 +28,10 @@ export default function Home() {
     if (!router.query.vaga) return;
     onOpen();
   }, [onOpen, router.query.vaga]);
+
+  useEffect(() => {
+    refetchListJob();
+  }, [refetchListJob, search]);
 
   return (
     <>
@@ -51,52 +50,13 @@ export default function Home() {
           gap={{ base: 2, md: 4, lg: 6 }}
           flexDirection={{ base: "column", lg: "row" }}
         >
-          <Box>
-            <Filter />
-          </Box>
+          <Filter />
           <Flex flexDirection="column" gap={{ base: 2, md: 4, lg: 6 }} w="full">
-            <Box>
-              <Flex
-                bg="white"
-                p={{ base: "2", md: "4", lg: "6" }}
-                border="1px"
-                borderColor={useColorModeValue("gray.200", "gray.700")}
-                borderRadius={{ base: "md", lg: "2xl" }}
-              >
-                <Input placeholder="Procurar vagas por título, empresa ou qualquer palavra chave..." />
-                <IconButton
-                  aria-label="Procurador de vagas"
-                  icon={<Search2Icon />}
-                  ml="2"
-                />
-              </Flex>
-            </Box>
-            <VStack
-              p={{ base: "2", md: "4", lg: "6" }}
-              gap={{ base: 0, md: 2, lg: 4 }}
-              bg="white"
-              border="1px"
-              borderColor={useColorModeValue("gray.200", "gray.700")}
-              borderRadius={{ base: "md", lg: "2xl" }}
-              alignItems={"stretch"}
-            >
-              {listJob?.map((job, key) => (
-                <CardVacancy
-                  index={key}
-                  key={key}
-                  job={job}
-                  isLoading={isLoadingListJob}
-                />
-              ))}
-
-              {!!job && !!router.query.vaga && (
-                <ModalApplyVacancy
-                  isOpen={isOpen}
-                  onClose={onClose}
-                  job={job}
-                />
-              )}
-            </VStack>
+            <Search onSearch={(value) => setSearch(value)} />
+            <WrapListVacancies jobs={listJob} isLoading={isLoadingListJob} />
+            {!!job && !!router.query.vaga && (
+              <ModalApplyVacancy isOpen={isOpen} onClose={onClose} job={job} />
+            )}
           </Flex>
         </Flex>
       </Container>
