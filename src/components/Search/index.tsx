01e9fe4321367727cead08272233d40b70e5ca55
vicente_nascimento <vicente19981@live.com>
2023-02-21 00:19:50 -0300

    feat: adding update search

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,23 +1,33 @@
-import { useDebounce } from "@/utils/debounce";
 import { Search2Icon } from "@chakra-ui/icons";
 import { Flex, IconButton, Input, useColorModeValue } from "@chakra-ui/react";
-import React, { useEffect, useState } from "react";
+import { useFilter } from "../Filter/context";
+import { VscLoading } from "react-icons/vsc";
+import React from "react";
+import { motion } from "framer-motion";
 
-type Props = {
-  onSearch: (value: string) => void;
-};
+const Search: React.FC = () => {
+  const { updateSearch, isLoading } = useFilter();
 
-const Search: React.FC<Props> = ({ onSearch }) => {
-  const [search, setSearch] = useState("");
+  const handleSubmit = (e: React.SyntheticEvent) => {
+    e.preventDefault();
 
-  const debounce = useDebounce(onSearch, 2000);
+    const target = e.target as typeof e.target & {
+      search: { value: string };
+    };
 
-  useEffect(() => {
-    debounce(search);
-  }, [debounce, search]);
+    updateSearch(target.search.value);
+  };
+
+  const onSearchClear = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value.length === 0) {
+      updateSearch("");
+    }
+  };
 
   return (
     <Flex
+      as={"form"}
+      onSubmit={handleSubmit}
       bg="white"
       p={{ base: "2", md: "4", lg: "6" }}
       border="1px"
@@ -25,14 +35,33 @@ const Search: React.FC<Props> = ({ onSearch }) => {
       borderRadius={{ base: "md", lg: "2xl" }}
     >
       <Input
-        onChange={(e) => setSearch(e.target.value)}
+        onChange={onSearchClear}
+        name="search"
+        disabled={isLoading}
         placeholder="Procurar vagas por título, empresa ou qualquer palavra chave..."
       />
-      <IconButton
-        aria-label="Procurador de vagas"
-        icon={<Search2Icon />}
-        ml="2"
-      />
+      {isLoading ? (
+        <IconButton
+          type="submit"
+          aria-label="Procurador de vagas"
+          icon={
+            <motion.span
+              animate={{ rotate: 180 }}
+              transition={{ repeat: Infinity, duration: 0.2 }}
+            >
+              <VscLoading />
+            </motion.span>
+          }
+          ml="2"
+        />
+      ) : (
+        <IconButton
+          type="submit"
+          aria-label="Procurador de vagas"
+          icon={<Search2Icon />}
+          ml="2"
+        />
+      )}
     </Flex>
   );
 };
