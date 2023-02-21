657baa4d7830d7f70ff0145b8f3fc106b1492e5a
vicente_nascimento <vicente19981@live.com>
2023-02-21 00:18:07 -0300

    feat: adding clear param value

--- src/api/jobs.ts
+++ src/api/jobs.ts
@@ -2,13 +2,23 @@ import { request } from "@/request";
 import { LoadAllJob, LoadJob } from "@/domain";
 
 export const getJobs = async (
-  params: LoadAllJob.Params
-): Promise<LoadAllJob.Model> =>
-  (
+  params?: LoadAllJob.Params
+): Promise<LoadAllJob.Model> => {
+  return (
     await request.get<{ data: LoadAllJob.Model }>(`/jobs`, {
-      params: params,
+      params: params && clearParamValueEmpty(params),
     })
   ).data.data;
+};
 
 export const getJob = async (jobId: String): Promise<LoadJob.Model> =>
   (await request.get(`/jobs/${jobId}`)).data;
+
+const clearParamValueEmpty = (params: LoadAllJob.Params) => {
+  for (const [key, value] of Object.entries(params)) {
+    if (!value) {
+      delete params[key as keyof LoadAllJob.Params];
+    }
+  }
+  return params;
+};
