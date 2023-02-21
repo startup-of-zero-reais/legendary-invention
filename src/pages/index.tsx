b549c075d79301d64cfca5b3aabcaf6d3a8f35bd
vicente_nascimento <vicente19981@live.com>
2023-02-21 00:19:03 -0300

    feat: wrapper in a provider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,16 @@
 import Head from "next/head";
 import { Header, Filter, ModalApplyVacancy, Search } from "@/components";
-import { Container, Flex, useDisclosure } from "@chakra-ui/react";
+import { Box, Container, Flex, useDisclosure } from "@chakra-ui/react";
 import { CONSTANTS } from "@/constants";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import { useLoadJob, useLoadAllJob } from "@/domain";
+import { useEffect } from "react";
+import { useLoadJob } from "@/domain";
 import WrapListVacancies from "@/components/WrapListVacancies";
+import { FilterProvider } from "@/components/Filter/context";
 
 export default function Home() {
   let router = useRouter();
   const { onOpen, isOpen, onClose } = useDisclosure();
-  const [search, setSearch] = useState("");
-
-  const {
-    data: listJob,
-    isLoading: isLoadingListJob,
-    refetch: refetchListJob,
-  } = useLoadAllJob({
-    search,
-  });
 
   const { data: job } = useLoadJob({
     id: router.query.vaga as string,
@@ -28,10 +20,6 @@ export default function Home() {
     if (!router.query.vaga) return;
     onOpen();
   }, [onOpen, router.query.vaga]);
-
-  useEffect(() => {
-    refetchListJob();
-  }, [refetchListJob, search]);
 
   return (
     <>
@@ -44,21 +32,33 @@ export default function Home() {
       <Header />
 
       <Container maxW="container.lg" minHeight="calc(100vh - 68px)">
-        <Flex
-          minH="100%"
-          p={{ base: "2", md: "4", lg: "6" }}
-          gap={{ base: 2, md: 4, lg: 6 }}
-          flexDirection={{ base: "column", lg: "row" }}
-        >
-          <Filter />
-          <Flex flexDirection="column" gap={{ base: 2, md: 4, lg: 6 }} w="full">
-            <Search onSearch={(value) => setSearch(value)} />
-            <WrapListVacancies jobs={listJob} isLoading={isLoadingListJob} />
-            {!!job && !!router.query.vaga && (
-              <ModalApplyVacancy isOpen={isOpen} onClose={onClose} job={job} />
-            )}
+        <FilterProvider>
+          <Flex
+            minH="100%"
+            p={{ base: "2", md: "4", lg: "6" }}
+            gap={{ base: 2, md: 4, lg: 6 }}
+            flexDirection={{ base: "column", lg: "row" }}
+          >
+            <Box>
+              <Filter />
+            </Box>
+            <Flex
+              flexDirection="column"
+              gap={{ base: 2, md: 4, lg: 6 }}
+              w="full"
+            >
+              <Search />
+              <WrapListVacancies />
+              {!!job && !!router.query.vaga && (
+                <ModalApplyVacancy
+                  isOpen={isOpen}
+                  onClose={onClose}
+                  job={job}
+                />
+              )}
+            </Flex>
           </Flex>
-        </Flex>
+        </FilterProvider>
       </Container>
     </>
   );
