3e2608a983406054d9aa537147ef89f739e3254f
vicente_nascimento <vicente19981@live.com>
2023-03-04 00:13:10 -0300

    feat: export usecase

--- src/domain/usecases/index.ts
+++ src/domain/usecases/index.ts
@@ -3,3 +3,5 @@ export * from "./load-job-list";
 export * from "./authentication";
 export * from "./load-job";
 export * from "./load-me";
+export * from "./register-candidate";
+export * from "./register-recruiter";
