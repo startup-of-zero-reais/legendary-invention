10eb771ec2e7075e345d3f6f1a41758f11d071d6
vicente_nascimento <vicente19981@live.com>
2023-03-04 00:10:52 -0300

    feat: adding register recruiter and candidate

--- src/server-lib/api/auth.ts
+++ src/server-lib/api/auth.ts
@@ -1,4 +1,5 @@
-import { Authentication, LoadMe } from "@/domain";
+import { Authentication, LoadMe, RegisterCandidate } from "@/domain";
+import { RegisterRecruiter } from "@/domain/usecases/register-recruiter";
 import { request } from "@/server-lib/services";
 
 export const auth = async (auth?: Authentication.Params) => {
@@ -22,16 +23,27 @@ export const me = async (sessionToken?: string): Promise<LoadMe.Model> => {
   }
 
   function handleError(err: Error) {
-    console.log(`fail getting me reason: ${err.message}`)
-    return { data: { candidate: undefined, recruiter: undefined } }
+    console.log(`fail getting me reason: ${err.message}`);
+    return { data: { candidate: undefined, recruiter: undefined } };
   }
 
-  const meResult = await request.get<LoadMe.Model>(`/auth/me`, requestConfig)
+  const meResult = await request
+    .get<LoadMe.Model>(`/auth/me`, requestConfig)
     .catch(handleError);
 
-  return meResult.data
+  return meResult.data;
 };
 
 export const logout = async (): Promise<void> => {
   await request.post<void>(`/auth/logout`);
+};
+
+export const registerCandidate = async (params: RegisterCandidate.Params) => {
+  return (await request.post<RegisterCandidate.Model>(`/candidates`, params))
+    .data;
+};
+
+export const registerRecruiter = async (params: RegisterRecruiter.Params) => {
+  return (await request.post<RegisterRecruiter.Model>(`/recruiters`, params))
+    .data;
 };
