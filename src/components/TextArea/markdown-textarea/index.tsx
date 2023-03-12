d3d307321e01f4b9082e332655c057ce594e0bbc
Jean Molossi <jean.carlosmolossi@gmail.com>
2023-03-12 02:13:22 -0300

    fix: change invalid colors

--- src/components/TextArea/markdown-textarea/index.tsx
+++ src/components/TextArea/markdown-textarea/index.tsx
@@ -4,10 +4,11 @@ import "@uiw/react-markdown-preview/markdown.css";
 import dynamic from "next/dynamic";
 import rehypeSanitize from "rehype-sanitize";
 import Code from "./mermaid-support";
-import { Button, Flex, FormControl, FormLabel } from "@chakra-ui/react";
+import { Button, Flex, FormControl, FormErrorMessage, FormLabel, ScaleFade } from "@chakra-ui/react";
 import { ReactNode, useCallback, useState } from "react";
 import { rewriteAnchor } from "./rewrite-anchors";
 import { toolbar } from "./toolbar";
+import { Control, Controller, useController } from "react-hook-form";
 
 const MDEditor = dynamic(
     () => import("@uiw/react-md-editor"),
@@ -17,7 +18,9 @@ const MDEditor = dynamic(
 interface MarkdownTextAreaProps {
     name: string;
     label: string;
-    onChange?: (event: any) => void;
+    control: Control<any>;
+    errorMessage?: string;
+    isInvalid?: boolean;
 }
 
 const defaultJobDescription = `Insira uma breve descrição da sua vaga
@@ -48,55 +51,78 @@ Você gostaria de acrescentar algo a mais sobre a vaga ?
 export const MarkdownTextArea = ({
     name,
     label,
-    onChange,
+    control,
+    errorMessage: errMessage = '',
+    isInvalid: invalid = false,
 }: MarkdownTextAreaProps) => {
     const [value, setValue] = useState(defaultJobDescription)
+
+    const isInvalid = invalid || value == defaultJobDescription;
+    const errorMessage = errMessage || 'A descrição é obrigatória';
     
     const [preview, setPreview] = useState<'edit' | 'preview'>('edit')
+
+    if (typeof window !== 'undefined') {
+        const editor = document?.querySelector('.wmde-markdown-var') as HTMLElement
+        if (editor && isInvalid)    
+            editor.style.setProperty('--color-border-default', '#FB3C4E')
+        else if (editor && !isInvalid)
+            editor.style.setProperty('--color-border-default', '#d0d7de')
+    }
 
     const togglePreview = useCallback(() => {
         setPreview(preview === 'edit' ? 'preview' : 'edit')
     }, [preview])
 
-    const internOnChange = useCallback((value: any, event: any) => {
-        onChange?.(event)
-        setValue(value ?? '');
-    }, [onChange])
-
     return (
         <Flex w="full" alignItems={"stretch"} flexDir="column" gap={2}>
-            <FormControl mt={2}>
+            <FormControl isInvalid={isInvalid}>
                 <FormLabel htmlFor={name}>{label}</FormLabel>
+
+                <Controller
+                    render={({ field }) => (
+                        <>
+                            <MDEditor
+                                id={name}
+                                data-color-mode="light"
+                                height={400}
+                                maxHeight={800}
+                                preview={preview}
+                                value={value}
+                                commands={toolbar()}
+                                extraCommands={[]}
+                                previewOptions={{
+                                    components: Code as any,
+                                    rehypePlugins: [
+                                        [rehypeSanitize],
+                                        rewriteAnchor(),
+                                    ]
+                                }}
+                                textareaProps={field}
+                                onChange={(value) => setValue(value || '')}
+                            />
+                        </>
+                    )}
+                    name={name}
+                    control={control}
+                />
+
+                <Flex w={"full"} justifyContent="space-between" mt={2}>
+                    <ScaleFade in={isInvalid}>
+                        <FormErrorMessage>
+                            {errorMessage}
+                        </FormErrorMessage>
+                    </ScaleFade>
+
+                    <Button
+                        onClick={togglePreview}
+                        colorScheme="linkedin"
+                        variant="outline"
+                    >
+                        {preview === 'edit' ? 'Visualizar' : 'Editar'}
+                    </Button>
+                </Flex>
             </FormControl>
-
-            <MDEditor
-                id={name}
-                data-color-mode="light"
-                height={400}
-                maxHeight={800}
-                preview={preview}
-                onChange={internOnChange}
-                value={value}
-                commands={toolbar()}
-                extraCommands={[]}
-                previewOptions={{
-                    components: Code as any,
-                    rehypePlugins: [
-                        [rehypeSanitize],
-                        rewriteAnchor(),
-                    ]
-                }}
-            />
-
-            <Flex w={"full"} justifyContent="flex-end">
-                <Button
-                    onClick={togglePreview}
-                    colorScheme="linkedin"
-                    variant="outline"
-                >
-                    {preview === 'edit' ? 'Visualizar' : 'Editar'}
-                </Button>
-            </Flex>
         </Flex>
     )
 }
